6ba48b82c9fe6cdeb7f7adb6bb6cd1468cf0d1c9
Gezi-lzq <lzqtxwd@gmail.com>
2025-01-09 14:51:07 +0800

    chore: remove Helm v2 installation and update ChartMuseum installation method

--- scripts/setup_test_environment.sh
+++ scripts/setup_test_environment.sh
@@ -16,28 +16,9 @@ export TEST_V3_XDG_DATA_HOME="$PWD/.helm3/xdg/data"
 [ "$(uname)" == "Darwin" ] && PLATFORM="darwin" || PLATFORM="linux"
 
 main() {
-    install_helm_v2
     install_helm_v3
     install_chartmuseum
     package_test_charts
-}
-
-install_helm_v2() {
-    if [ ! -f "testbin/helm2" ]; then
-        mkdir -p testbin/
-        TARBALL="helm-${HELM_V2_VERSION}-${PLATFORM}-amd64.tar.gz"
-        wget "https://get.helm.sh/${TARBALL}"
-        tar -C testbin/ -xzf $TARBALL
-        rm -f $TARBALL
-        pushd testbin/
-        UNCOMPRESSED_DIR="$(find . -mindepth 1 -maxdepth 1 -type d)"
-        mv $UNCOMPRESSED_DIR/helm .
-        rm -rf $UNCOMPRESSED_DIR
-        chmod +x ./helm
-        mv ./helm ./helm2
-        popd
-        HELM_HOME=${TEST_V2_HELM_HOME} helm2 init --client-only
-    fi
 }
 
 install_helm_v3() {
@@ -61,21 +42,13 @@ install_chartmuseum() {
     if [ ! -f "testbin/chartmuseum" ]; then
         mkdir -p testbin/
         pushd testbin/
-        wget "https://s3.amazonaws.com/chartmuseum/release/${CHARTMUSEUM_VERSION}/bin/${PLATFORM}/amd64/chartmuseum"
+        curl https://raw.githubusercontent.com/helm/chartmuseum/main/scripts/get-chartmuseum | bash
         chmod +x ./chartmuseum
         popd
     fi
 }
 
 package_test_charts() {
-    pushd testdata/charts/helm2/
-    for d in $(find . -maxdepth 1 -mindepth 1 -type d); do
-        pushd $d
-        HELM_HOME=${TEST_V2_HELM_HOME} helm2 package --sign --key helm-test --keyring ../../../pgp/helm-test-key.secret .
-        popd
-    done
-    popd
-
     pushd testdata/charts/helm3/
     for d in $(find . -maxdepth 1 -mindepth 1 -type d); do
         pushd $d
